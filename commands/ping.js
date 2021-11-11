ac8d3abe14c51a72c5cea422ec499d84e076198a
vdalonso <vdalonso@ucsd.edu>
2021-11-11 13:51:23 -0800

    removed javascript stuff

--- commands/ping.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    name: 'ping',
-    description: "check if i'm alive! :D",
-    execute(message, args, x){
-        x.a = 1;
-        x.b = 11;
-        message.channel.send('pong!');
-    }
-}
